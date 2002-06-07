82fd1ac429217951069e59a6b8063527d9839ed8
lars <lars@cae33c24-edb7-11db-8150-7705e9bc5c8f>
2002-06-07 16:48:09 +0000

    Simple call forwarding test.
    git-svn-id: file:///Library/svn/aculab/trunk@10 cae33c24-edb7-11db-8150-7705e9bc5c8f

--- python/callin.py
+++ python/callin.py
@@ -17,58 +17,148 @@ for o, a in options:
     else:
         usage()
 
-inparms = aculab.IN_XPARMS()
-inparms.net = port
-inparms.ts = -1
+class CallEventDispatcher:
 
-aculab.call_openin(inparms)
+    def __init__(self):
+        self.calls = {}
+
+    # must only be called from a dispatched event
+    # - not from a separate thread
+    def add(self, call):
+        self.calls[call.handle] = call
+
+    # must only be called from a dispatched event
+    # - not from a separate thread
+    def remove(self, call):
+        del self.calls[call.handle]
+
+    def run(self):
+        event = aculab.STATE_XPARMS()
+        
+        while 1:
+            if not self.calls:
+                return
+            
+            event.handle = 0
+            event.timeout = 1000
+
+            rc = aculab.call_event(event)
+            # call the event handler
+            if event.handle:
+                call = self.calls[event.handle]
+                ev = names.event[event.state].lower()
+                print hex(event.handle), ev
+                # if the call has an observer, call it, too
+                if call.observer and hasattr(call.observer, ev):
+                    call.observer.__class__.__dict__[ev](call.observer, call)
+                call.__class__.__dict__[ev](call)
+
+dispatcher = CallEventDispatcher()
+
+class Call:
+
+    # restart is silently ignored for outbound calls
+    def __init__(self, port = 0, timeslot = -1, number = '',
+                 observer = None, restart = 1):
+        
+        self.port = port
+        self.timeslot = timeslot
+        self.observer = observer
+        
+        if number:
+            self.restart = 0
+            self.openout(number)
+        else:
+            self.restart = restart
+            self.openin()
+            
+    def openin(self):
+        inparms = aculab.IN_XPARMS()
+        inparms.net = self.port
+        inparms.ts = self.timeslot
+        inparms.cnf = aculab.CNF_REM_DISC | aculab.CNF_TSPREFER
+
+        aculab.call_openin(inparms)
+        self.handle = inparms.handle
+
+        dispatcher.add(self)
+
+    def openout(self, number):
+        self.number = number
+        
+        outparms = aculab.OUT_XPARMS()
+        outparms.net = self.port
+        outparms.ts = self.timeslot
+        outparms.cnf = aculab.CNF_REM_DISC | aculab.CNF_TSPREFER
+        outparms.sending_complete = 1
+        outparms.originating_address = '3172542'
+        outparms.destination_address = number
+
+        aculab.call_openout(outparms)
+        self.handle = outparms.handle
+
+        dispatcher.add(self)
+
+    def disconnect(self, cause = 0):
+        causeparms = aculab.CAUSE_XPARMS()
+        causeparms.handle = self.handle
+        causeparms.cause = cause
+        aculab.call_disconnect(causeparms)
+        
+    def ev_ext_facility(self):
+        pass
+
+    def ev_incoming_call_det(self):
+        details = aculab.DETAIL_XPARMS()
+        details.handle = self.handle
+
+        aculab.call_details(details)
+
+        print 'stream: %d timeslot: %d' % (details.stream, details.ts)
     
-handle = inparms.handle
+        aculab.call_accept(self.handle)
 
-event = aculab.STATE_XPARMS()
+    def ev_wait_for_outgoing(self):
+        pass
 
-def ev_ext_facility(handle):
-    pass
+    def ev_outgoing_ringing(self):
+        pass
 
-def ev_incoming_call_det(handle):
-    details = aculab.DETAIL_XPARMS()
-    details.handle = handle
+    def ev_call_connected(self):
+        pass
 
-    aculab.call_details(details)
+    def ev_remote_disconnect(self):
+        self.disconnect()
 
-    print 'stream: %d timeslot: %d' % (details.stream, details.ts)
-    
-    aculab.call_accept(handle)
+    def ev_idle(self):
+        dispatcher.remove(self)
+        cause = aculab.CAUSE_XPARMS()
+        cause.handle = self.handle
+        aculab.call_release(cause)
 
-def ev_call_connected(handle):
-    pass
-
-def ev_remote_disconnect(handle):
-    aculab.call_disconnect(handle)
-
-def ev_idle(handle):
-    cause = aculab.CAUSE_XPARMS()
-    cause.handle = handle
-    aculab.call_release(cause)
-
-    inparms = aculab.IN_XPARMS()
-    inparms.net = port
-    inparms.ts = -1
-
-    aculab.call_openin(inparms)
-    
-    handle = inparms.handle
+        if not hasattr(self, 'number') and self.restart:
+            self.openin()
+        else:
+            if hasattr(self, 'number'):
+                del self.number
 
 
-last_event = None
-while 1:
-    event.handle = handle
-    event.timeout = 1000
+class TestForward:
 
-    rc = aculab.call_event(event)
-    # call the event handler
-    if last_event != event.state:
-        print names.event[event.state].lower()
-        __main__.__dict__[names.event[event.state].lower()](handle)
-        last_event = event.state
+    def __init__(self):
+        self.incall = Call(port, 1, '', self, 0)
 
+    def ev_incoming_call_det(self, call):
+        self.outcall = Call(port, 2, '123', self)
+
+    def ev_remote_disconnect(self, call):
+        if call == self.incall:
+            self.outcall.disconnect()
+        else:
+            self.incall.disconnect()
+
+# c = Call(port)
+
+t = TestForward()
+
+dispatcher.run()
