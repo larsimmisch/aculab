be51068c611ce0da841c3d070973eaab5760805b
lars <lars@cae33c24-edb7-11db-8150-7705e9bc5c8f>
2002-06-07 23:41:18 +0000

    Added controller classes.
    git-svn-id: file:///Library/svn/aculab/trunk@11 cae33c24-edb7-11db-8150-7705e9bc5c8f

--- python/callin.py
+++ python/callin.py
@@ -42,34 +42,70 @@ class CallEventDispatcher:
             event.handle = 0
             event.timeout = 1000
 
+            handled = 0
+
             rc = aculab.call_event(event)
-            # call the event handler
+
+            handled = ''
+            
+            # call the event handlers
             if event.handle:
                 call = self.calls[event.handle]
                 ev = names.event[event.state].lower()
-                print hex(event.handle), ev
-                # if the call has an observer, call it, too
-                if call.observer and hasattr(call.observer, ev):
-                    call.observer.__class__.__dict__[ev](call.observer, call)
-                call.__class__.__dict__[ev](call)
+                # let the call handle events first
+                try:
+                    m = getattr(call, ev)
+                    m()
+                    handled = '(call'
+                except AttributeError:
+                    if hasattr(call, ev):
+                        raise
+                except:
+                    raise
+
+                # pass the event on to the controller
+                try:
+                    m = getattr(call.controller, ev)
+                    m(call)
+                    if handled:
+                        handled += ', controller)'
+                    else:
+                        handled = '(controller)'
+                except AttributeError:
+                    if handled:
+                        handled += ')'
+                    if hasattr(call.controller, ev):
+                        raise
+                except:
+                    raise
+                
+                if not handled:
+                    handled = '(ignored)'
+
+                print hex(event.handle), ev, handled
+
 
 dispatcher = CallEventDispatcher()
 
+# The CallHandle class models a call handle, as defined by Aculab,
+# and common operations on it. Event handling is delegated to the controller,
+# in classical OO decomposition.
+
 class Call:
 
-    # restart is silently ignored for outbound calls
-    def __init__(self, port = 0, timeslot = -1, number = '',
-                 observer = None, restart = 1):
-        
+    def __init__(self, controller, port = 0, number = '', timeslot = -1):
+        self.controller = controller
         self.port = port
-        self.timeslot = timeslot
-        self.observer = observer
-        
         if number:
-            self.restart = 0
-            self.openout(number)
+            self.number = number
+        self.timeslot = timeslot
+
+        self.restart()
+
+    def restart(self):
+        if hasattr(self, 'number'):
+            self.openout()
         else:
-            self.restart = restart
             self.openin()
             
     def openin(self):
@@ -83,73 +119,106 @@ class Call:
 
         dispatcher.add(self)
 
-    def openout(self, number):
-        self.number = number
-        
+    def openout(self):
         outparms = aculab.OUT_XPARMS()
         outparms.net = self.port
         outparms.ts = self.timeslot
         outparms.cnf = aculab.CNF_REM_DISC | aculab.CNF_TSPREFER
         outparms.sending_complete = 1
         outparms.originating_address = '3172542'
-        outparms.destination_address = number
+        outparms.destination_address = self.number
 
         aculab.call_openout(outparms)
         self.handle = outparms.handle
 
         dispatcher.add(self)
 
+    def listen_to(sink, source):
+        "sink and source are tuples of timeslots"
+        output = aculab.OUTPUT_PARMS()
+        output.ost = sink[0]
+        output.ots = sink[1]
+        output.mode = aculab.CONNECT_MODE
+        output.ist = source[0]
+        output.its = source[1]
+
+        sw = call_port_2_swdrvr(self.port)
+
+        sw_set_output(sw, output)
+
+    def get_details(self):
+        self.details = aculab.DETAIL_XPARMS()
+        self.details.handle = self.handle
+
+        aculab.call_details(self.details)
+
+        return self.details
+
+    def accept(self):
+        aculab.call_accept(self.handle)        
+
     def disconnect(self, cause = 0):
-        causeparms = aculab.CAUSE_XPARMS()
-        causeparms.handle = self.handle
-        causeparms.cause = cause
-        aculab.call_disconnect(causeparms)
-        
-    def ev_ext_facility(self):
-        pass
+        if self.handle:
+            causeparms = aculab.CAUSE_XPARMS()
+            causeparms.handle = self.handle
+            causeparms.cause = cause
+            aculab.call_disconnect(causeparms)
 
     def ev_incoming_call_det(self):
-        details = aculab.DETAIL_XPARMS()
-        details.handle = self.handle
-
-        aculab.call_details(details)
-
-        print 'stream: %d timeslot: %d' % (details.stream, details.ts)
-    
-        aculab.call_accept(self.handle)
-
-    def ev_wait_for_outgoing(self):
-        pass
+        self.get_details()
 
     def ev_outgoing_ringing(self):
-        pass
-
-    def ev_call_connected(self):
-        pass
-
-    def ev_remote_disconnect(self):
-        self.disconnect()
+        self.get_details()
 
     def ev_idle(self):
         dispatcher.remove(self)
         cause = aculab.CAUSE_XPARMS()
         cause.handle = self.handle
+
+        self.handle = None
+
+        if hasattr(self, 'details'):
+            del self.details
+
+        if hasattr(self, 'number'):
+            del self.number
+
         aculab.call_release(cause)
-
-        if not hasattr(self, 'number') and self.restart:
-            self.openin()
-        else:
-            if hasattr(self, 'number'):
-                del self.number
-
-
-class TestForward:
-
-    def __init__(self):
-        self.incall = Call(port, 1, '', self, 0)
+        
+class IncomingCallController:
 
     def ev_incoming_call_det(self, call):
-        self.outcall = Call(port, 2, '123', self)
+        print hex(call.handle), 'stream: %d timeslot: %d' \
+              % (call.details.stream, call.details.ts)
+
+        call.accept()
+        
+    def ev_remote_disconnect(self, call):
+        call.disconnect()
+
+class RepeatedIncomingCallController(IncomingCallController):
+
+    def ev_idle(self, call):
+        call.restart()
+        
+class OutgoingCallController:
+
+    def ev_outgoing_ringing(self, call):
+        print hex(call.handle), 'stream: %d timeslot: %d' \
+              % (call.details.stream, call.details.ts)
+
+    def ev_remote_disconnect(self, call):
+        call.disconnect()
+
+class ForwardCallController:
+
+    def __init__(self):
+        self.incall = None
+        self.outcall = None
+
+    def ev_incoming_call_det(self, call):
+        self.incall = call
+        self.outcall = Call(self, port, '123', 2)
 
     def ev_remote_disconnect(self, call):
         if call == self.incall:
@@ -157,8 +226,21 @@ class TestForward:
         else:
             self.incall.disconnect()
 
-# c = Call(port)
+        call.disconnect()
 
-t = TestForward()
+    def ev_idle(self, call):
+        if call == self.incall:
+            self.outcall.disconnect()
+        else:
+            self.incall.disconnect()
+
+icc = IncomingCallController()
+ricc = RepeatedIncomingCallController()
+occ = OutgoingCallController()
+fcc = ForwardCallController()
+
+c = Call(fcc, port)
+
+# t = TestForward()
 
 dispatcher.run()
