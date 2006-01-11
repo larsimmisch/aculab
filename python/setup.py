185e52cbc0eee91686bf591f6b5430750f188385
lars <lars@cae33c24-edb7-11db-8150-7705e9bc5c8f>
2006-01-11 23:44:54 +0000

    Working with v5 and v6 (so far).
    git-svn-id: file:///Library/svn/aculab/trunk@179 cae33c24-edb7-11db-8150-7705e9bc5c8f

--- python/setup.py
+++ python/setup.py
@@ -8,27 +8,58 @@ def macroify(m):
     else:
         return '-D%s' % m[0]
 
+extra_objects = None
+
 if os.name == 'nt':
     raise RuntimeError('Windows not supported yet')
 elif os.name == 'posix':
     dtk = os.getenv('ACULAB_ROOT')
-    fax = '/ProsodyLibraries/Group3fax_LINUX_V6_rel321/API'
-    define_macros = [('ACU_LINUX', None),
-                     ('SM_POLL_UNIX', None),
-                     ('TiNGTYPE_LINUX', None),
-                     ('TiNG_USE_V6', None)]
-    include_dirs = [dtk + '/include',
-                    dtk + '/ting/include',
-                    dtk + fax + '/include' ]
-    extra_objects = [dtk + '/ting/libutil/gen-LINUX_V6/aculog.o',
-                     dtk + '/ting/libutil/gen-LINUX_V6/vseprintf.o',   
-                     dtk + '/ting/libutil/gen-LINUX_V6/bfile.o',   
-                     dtk + '/ting/libutil/gen-LINUX_V6/bfopen.o',
-                     dtk + fax + '/lib/actiff.o',
-                     dtk + fax + '/lib/faxlib.o' ]
-    lib_dirs = [dtk + '/lib']
-    libs = ['acu_cl', 'acu_res', 'TiNG', 'acu_common', 'stdc++']
-    sources = ["lowlevel.i"]
+    if not dtk:
+        raise ValueError('ACULAB_ROOT is not defined')
+
+    # crude detection of V5 vs v6
+    if os.path.exists(dtk + '/include/cl_lib.h'):
+        # Version 6
+        version = '6.0'
+        fax = '/ProsodyLibraries/Group3fax_LINUX_V6_rel321/API'
+        if not os.path.exists(dtk + fax):
+            fax = None
+                      
+        define_macros = [('ACU_LINUX', None),
+                         ('SM_POLL_UNIX', None),
+                         ('TiNGTYPE_LINUX', None),
+                         ('TiNG_USE_V6', None)]
+        include_dirs = [dtk + '/include',
+                        dtk + '/ting/include']
+
+        extra_objects = [dtk + '/ting/libutil/gen-LINUX_V6/aculog.o',
+                         dtk + '/ting/libutil/gen-LINUX_V6/vseprintf.o',   
+                         dtk + '/ting/libutil/gen-LINUX_V6/bfile.o',   
+                         dtk + '/ting/libutil/gen-LINUX_V6/bfopen.o']
+        if fax:
+            define_macros.append(('HAVE_FAX', None))
+            include_dirs.append(dtk + fax + '/include')
+            extra_objs = extra_objs + [dtk + fax + '/lib/actiff.o',
+                                       dtk + fax + '/lib/faxlib.o']
+            
+        lib_dirs = [dtk + '/lib']
+        libs = ['acu_cl', 'acu_res', 'TiNG', 'acu_common', 'stdc++']
+        sources = ["lowlevel.i"]
+    else:
+        # Version 5
+        version = '5.0' 
+        define_macros = [('ACU_LINUX', None),
+                         ('SM_POLL_UNIX', None)]
+
+        include_dirs = [dtk + '/call/include',
+                        dtk + '/switch/include',
+                        dtk + '/speech/include']
+
+        lib_dirs = [dtk + '/call/lib',
+                    dtk + '/switch/lib',
+                    dtk + '/speech/lib']
+        libs = ['mvcl', 'mvsw', 'mvsm']
+        sources = ["lowlevel.i"]
     
 swig_opts = ['-modern', '-new_repr'] + \
             [macroify(d) for d in define_macros] + \
@@ -37,7 +68,7 @@ swig_opts = ['-modern', '-new_repr'] + \
 from distutils.core import setup,Extension
 
 setup (name = "aculab",
-	   version = "2.0",
+	   version = version,
 	   description = "Aculab Python wrapper",
 	   author = "Lars Immisch",
 	   author_email = "lars@ibp.de",
