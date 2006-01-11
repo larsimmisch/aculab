e2f2a0efe68d05113bc3027f64d0da777d6dae4b
lars <lars@cae33c24-edb7-11db-8150-7705e9bc5c8f>
2006-01-11 22:20:48 +0000

    Look for Python include/libs in /usr and /usr/local
    git-svn-id: file:///Library/svn/aculab/trunk@178 cae33c24-edb7-11db-8150-7705e9bc5c8f

--- python/unix_v5.mk
+++ python/unix_v5.mk
@@ -9,13 +9,19 @@ CC := gcc
 SWIG := swig
 PYTHON := python
 
-PYTHON_INCLUDE = -I/usr/include/python2.4
-PYTHON_LIBDIR	= -L/usr/lib/python2.4/config
+PYTHON_INCLUDE := -I$(firstword $(strip \
+						$(wildcard /usr/local/include/python2.4) \
+	                    $(wildcard /usr/include/python2.4)))
+
+PYTHON_LIBDIR := -L$(firstword $(strip \
+                       $(wildcard /usr/local/lib/python2.4/config/) \
+                       $(wildcard /usr/lib/python2.4/config/)))
+
 PYTHON_LIBS = -lpython2.4
 
 # TiNGTYPE := LINUX
 DEFINES := -DUNIX_SYSTEM -DSM_POLL_UNIX # -DTiNGTYPE_$(TiNGTYPE) -DHAVE_TiNG
-C_DEFINES := -g -DNDEBUG -fPIC $(DEFINES)
+C_DEFINES := -g -DNDEBUG -D_REENTRANT -fPIC $(DEFINES)
 
 ACULAB_INCLUDE = -I$(DTK)/call/include -I$(DTK)/switch/include \
 	-I$(DTK)/speech/include
