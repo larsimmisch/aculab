85f078527ef9b77cffd8bf82252ff987f39b8b82
lars <lars@cae33c24-edb7-11db-8150-7705e9bc5c8f>
2007-01-08 12:16:56 +0000

    Don't apply SIZED_STRUCT macro to ACU_SNAPSHOT_PARMS.
    git-svn-id: file:///Library/svn/aculab/trunk@230 cae33c24-edb7-11db-8150-7705e9bc5c8f

--- python/sized_struct.py
+++ python/sized_struct.py
@@ -5,13 +5,14 @@ import xml.sax.handler
 
 class FindStruct(xml.sax.handler.ContentHandler):
 
-    def __init__(self, file = sys.stdout):
+    def __init__(self, file = sys.stdout, exclude = []):
         self.in_class = 0
         self.in_typescope = 0
         self.in_cdecl = 0
         self.candidate = None
         self.locator = None
         self.file = file
+        self.exclude = exclude
 
     #Overridden DocumentHandler methods
     def setDocumentLocator(self, locator):
@@ -33,7 +34,8 @@ class FindStruct(xml.sax.handler.ContentHandler):
                 self.candidate = attrs['value']
             if self.in_cdecl:
                 if self.candidate and attrs['value'] == 'size':
-                    self.file.write('SIZED_STRUCT(%s)\n' % self.candidate)
+                    if not self.candidate in self.exclude:
+                        self.file.write('SIZED_STRUCT(%s)\n' % self.candidate)
 
     def endElement(self, name):
         if name == 'class':
@@ -52,10 +54,14 @@ def usage():
 if __name__ == '__main__':
     outfile = 'sized_struct.i'
 
-    options, args = getopt.getopt(sys.argv[1:], 'o:')
+    exclude = []
+
+    options, args = getopt.getopt(sys.argv[1:], 'o:x:')
     for o, a in options:
         if o == '-o':
             outfile = a
+        if o == '-x':
+            exclude.append(a)
         else:
             usage()
 
@@ -65,7 +71,7 @@ if len(args) != 1:
 
 of = open(outfile, 'w')
 parser = xml.sax.make_parser()
-handler = FindStruct(of)
+handler = FindStruct(of, exclude)
 
 parser.setContentHandler(handler)
 parser.parse(args[0])
